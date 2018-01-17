4e3b7ef78277f3c6f1987b834a948bafc2105a89
Ociel Coria-Jimenez <oc102892@gmail.com>
2018-01-17 17:48:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+  delete newObj[key]
+
+  return newObj
+}
