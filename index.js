a52a1ff6d8513838195a8c271d9abb2b621771db
Ociel Coria-Jimenez <oc102892@gmail.com>
2018-01-17 17:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipe = {}
 function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
